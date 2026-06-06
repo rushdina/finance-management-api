8bc1ce260cf8e249402fea31f89e721be58c0056
rushdina <redredrushrush@gmail.com>
2026-06-06 16:06:01 +0800

    Set up PostgreSQL database connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// Create Node.js Express backend server
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./db.js";
 
 dotenv.config();
 
@@ -10,7 +13,24 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Personal Management API is running");
+  res.send("Personal Finance Management API is running");
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()"); // retrieve current date and time
+    console.log(result);
+    res.json({
+      message: "Database connected successfully",
+      databaseTime: result.rows[0],
+    });
+  } catch (error) {
+    // Server error
+    res.status(500).json({
+      message: "Database connection failed",
+      error: error.message,
+    });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
