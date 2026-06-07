ccf2dcef0680a7765c38764850c192b8bcbfad19
rushdina <redredrushrush@gmail.com>
2026-06-07 18:27:09 +0800

    Added PUT transaction endpoint

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -49,8 +49,9 @@ export const getTransactionById = async (req, res) => {
     `,
       [id],
     );
+    console.log(result);
 
-    // Requested NOT FOUND
+    // Requested Resource NOT FOUND
     if (result.rows.length === 0) {
       return res.status(404).json({
         message: "Transaction not found",
@@ -89,6 +90,47 @@ export const createTransaction = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Failed to create transaction",
+      error: error.message,
+    });
+  }
+};
+
+// PUT /api/transactions/:id
+export const updateTransaction = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, amount, type, category_id, transaction_date } = req.body;
+
+    const result = await pool.query(
+      `
+      UPDATE transactions
+      SET
+        title = $1,
+        amount = $2,
+        type = $3,
+        category_id = $4,
+        transaction_date = $5
+      WHERE id = $6
+      RETURNING *
+    `,
+      [title, amount, type, category_id, transaction_date, id],
+    );
+    console.log(result);
+
+    // Requested Resource NOT FOUND
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Transaction not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Transaction updated successfully",
+      transaction: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update transaction",
       error: error.message,
     });
   }
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -3,6 +3,7 @@ import {
   getTransactions,
   getTransactionById,
   createTransaction,
+  updateTransaction,
 } from "../controllers/transactionController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/", getTransactions); // GET http://localhost:5000/api/transactions
 router.get("/:id", getTransactionById); // GET http://localhost:5000/api/transactions/1
 router.post("/", createTransaction); // POST http://localhost:5000/api/transactions
+router.put("/:id", updateTransaction);
 
 export default router;
